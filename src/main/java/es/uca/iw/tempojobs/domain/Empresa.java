47ec5e9775af0fc52e72424fcce9d448fb29498b
Matías Díaz <erithen@gmail.com>
2016-05-11 17:44:32 +0200

    sp1test4 v.inicial

---
+++ src/main/java/es/uca/iw/tempojobs/domain/Empresa.java
@@ -0,0 +1,38 @@
+package es.uca.iw.tempojobs.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Empresa {
+
+    /**
+     * Nombre
+     */
+    @Size(min = 3, max = 32)
+    private String sNombre;
+
+    /**
+     * CIF
+     */
+    @NotNull
+    @Size(min = 9, max = 16)
+    private String sCIF;
+
+    /**
+     * Actividad Profesional
+     */
+    @NotNull
+    @Size(min = 3, max = 64)
+    private String sActividadProfesional;
+
+    /**
+     * Número de Empleados
+     */
+    @NotNull
+    private Integer iEmpleados;
+}
