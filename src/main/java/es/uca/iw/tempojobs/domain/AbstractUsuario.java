7d374b2bca6611f1833d32167baee921e81ff4e2
Matías Díaz <erithen@gmail.com>
2016-05-12 17:39:15 +0200

    test inicial

---
+++ src/main/java/es/uca/iw/tempojobs/domain/AbstractUsuario.java
@@ -0,0 +1,64 @@
+package es.uca.iw.tempojobs.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public abstract class AbstractUsuario {
+
+    /**
+     * Nombre
+     */
+    @Size(min = 3, max = 32)
+    private String sNombre;
+
+    /**
+     * Apellidos
+     */
+    @NotNull
+    @Size(min = 3, max = 64)
+    private String sApellidos;
+
+    /**
+     * Fecha de Nacimiento
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date fFechaNac;
+
+    /**
+     * Dirección
+     */
+    @NotNull
+    @Size(min = 3, max = 128)
+    private String sDireccion;
+
+    /**
+     * Email
+     */
+    @NotNull
+    @Size(min = 6, max = 128)
+    private String sEmail;
+
+    /**
+     * Teléfono
+     */
+    @NotNull
+    @Size(min = 9, max = 16)
+    private String sTelefono;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 8, max = 128)
+    private String sPassword;
+}
---
+++ src/main/java/es/uca/iw/tempojobs/domain/Empleado.java
@@ -0,0 +1,39 @@
+package es.uca.iw.tempojobs.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Empleado extends AbstractUsuario {
+
+    /**
+     * Fecha comienzo
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date fInicio;
+
+    /**
+     * Fecha finalización
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date fFinal;
+
+    /**
+     * Puesto
+     */
+    @NotNull
+    @Size(min = 3, max = 32)
+    private String sPuesto;
+}
