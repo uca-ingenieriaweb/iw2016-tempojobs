40e153b7f9ede3c014c75cd34986b2ef6e845de5
Matías Díaz <erithen@gmail.com>
2016-05-30 15:02:51 +0200

    Pruebas

---
+++ src/main/java/es/uca/iw/tempojobs/domain/Sede.java
@@ -0,0 +1,56 @@
+package es.uca.iw.tempojobs.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToMany;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(finders = { "findSedesByLocalidadLikeOrProvinciaLike" })
+public class Sede {
+
+    /**
+     */
+    @Size(min = 3, max = 16)
+    private String nombre;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 128)
+    private String direccion;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 64)
+    private String localidad;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 32)
+    private String provincia;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 32)
+    private String pais;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 9, max = 16)
+    private String telefono;
+
+    /**
+     */
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<Oferta> ofertas = new HashSet<Oferta>();
+}
