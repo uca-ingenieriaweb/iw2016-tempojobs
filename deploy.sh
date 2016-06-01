ff46acbc21266ef8a0462131efe586456ed398a2
Matías Díaz <erithen@gmail.com>
2016-06-01 23:46:14 +0200

    Añadido apodo y clave

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,17 @@
 mysql -u root -p < src/sql/create.localdb.sql
 
 gvnix.sh script --file tempojobs.roo --lineNumbers
+
+echo 'Elija una opción:'
+echo '1 - Ejecutar tests'
+echo '2 - Ejecutar tomcat'
+echo -n 'opción: '
+readline opcion
+
+if [ "$opcion" == "1" ]; then
+	mvn clean compile test
+elif [ "$opcion" == "2" ]; then
+	mvn clean compile tomcat7:run
+else
+	"FIN SCRIPT"	
+fi
