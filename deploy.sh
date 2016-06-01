bbe4777d8ce6bf60bccfa94d4d9ff4bd5d117121
Matías Díaz <erithen@gmail.com>
2016-06-02 00:58:22 +0200

    Prueba de security

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,9 @@
 mysql -u root -p < src/sql/create.localdb.sql
 
 gvnix.sh script --file tempojobs.roo --lineNumbers
-quit
 
-echo 'Elija una opción:'
-echo '1 - Ejecutar tests'
-echo '2 - Ejecutar tomcat'
-echo -n 'opción: '
-readline opcion
-
-if [ "$opcion" == "1" ]; then
-	mvn clean compile test
-elif [ "$opcion" == "2" ]; then
-	mvn clean compile tomcat7:run
-else
-	"FIN SCRIPT"	
-fi
+echo 'Para ejecutar los test:'
+echo 'mvn clean compile test'
+echo ' '
+echo 'Para ejecutar aplicacion:'
+echo 'mvn clean compile tomcat7:run'
