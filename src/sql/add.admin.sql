1d371a91af011bdf7e9be15acec1751b1a8adc0b
Matías Díaz <erithen@gmail.com>
2016-06-02 14:17:44 +0200

    fix

--- src/sql/add.admin.sql
+++ src/sql/add.admin.sql
@@ -1,3 +1,3 @@
 INSERT INTO abstract_usuario VALUES
 ('Admin', 1, 'Admin', 'admin', 'administrador', 'localhost', 'admin@localhost', '1980-01-01',
-	'localhost', 'Admin', 'roo', '000 000 000', NULL, NULL, NULL, NULL, NULL, NULL);
+	'localhost', 'Admin', 'roo', '000 000 000', 0, NULL, NULL, NULL, NULL, NULL);
