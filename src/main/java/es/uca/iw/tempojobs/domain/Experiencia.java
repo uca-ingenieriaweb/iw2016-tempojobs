6bbe7592f7a0e2f3feab18fc0fc62444aa4c9e6e
Matías Díaz <erithen@gmail.com>
2016-06-02 10:58:27 +0200

    tempojobs.roo: error line 98

---
+++ src/main/java/es/uca/iw/tempojobs/domain/Experiencia.java
@@ -0,0 +1,42 @@
+package es.uca.iw.tempojobs.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Experiencia {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 32)
+    private String empresa;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 32)
+    private String puesto;
+
+    /**
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date fechaInicio;
+
+    /**
+     */
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date fechaFinal;
+}
