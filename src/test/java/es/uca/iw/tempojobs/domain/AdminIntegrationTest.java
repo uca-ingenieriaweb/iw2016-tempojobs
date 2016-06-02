2ae1d2ac3da30616598a1974b611bcab85cf8027
Matías Díaz <erithen@gmail.com>
2016-06-02 10:46:36 +0200

    tempojobs.roo: error linea 93

---
+++ src/test/java/es/uca/iw/tempojobs/domain/AdminIntegrationTest.java
@@ -0,0 +1,11 @@
+package es.uca.iw.tempojobs.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Admin.class)
+public class AdminIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
---
+++ src/test/java/es/uca/iw/tempojobs/domain/InscripcionIntegrationTest.java
@@ -0,0 +1,11 @@
+package es.uca.iw.tempojobs.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Inscripcion.class)
+public class InscripcionIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
